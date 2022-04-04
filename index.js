a546c67108858a1527b496b109678294b7dd3044
Your Name <you@example.com>
2022-04-04 09:53:41 -0400

    second push

---
+++ index.js
@@ -0,0 +1,10 @@
+const math = require('./math')
+const moment = require('moment');
+
+console.log(math.add(2,3))
+
+const time = new moment();
+
+console.log(time.format('h:mm:ss a'));
+
+
---
+++ math.js
@@ -0,0 +1,7 @@
+const add = (x,y) => x + y;
+const multiply = (x,y) => x * y;
+const extract = (x,y) => x - y;
+
+module.exports.add = add;
+module.exports.multiply = multiply;
+module.exports.extract = extract;
